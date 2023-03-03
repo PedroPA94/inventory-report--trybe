8516ef530e532c3e39055bf9ace0ccd32d853382
Pedro Papini <ppapinia@gmail.com>
2023-03-03 19:51:31 -0300

    Implement Json import

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -7,7 +8,10 @@ class Inventory:
     @classmethod
     def import_data(cls, path_to_file: str, report_type: str) -> str:
         with open(path_to_file, mode="r") as data:
-            products = list(csv.DictReader(data))
+            if path_to_file.endswith('.csv'):
+                products = list(csv.DictReader(data))
+            else:
+                products = json.load(data)
 
             if report_type == 'simples':
                 return SimpleReport.generate(products)
