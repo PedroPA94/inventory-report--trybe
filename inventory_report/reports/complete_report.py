c873e5bf6e63c7471447dcfe111672eeefda04a8
Pedro Papini <ppapinia@gmail.com>
2023-03-03 19:00:09 -0300

    Implement complete report

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,31 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.inventory.product import Product
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(cls, products: list[Product]) -> str:
+        companies_counter = CompleteReport.get_companies_counter(products)
+
+        return (
+            f"{super().generate(products)}\n"
+            "Produtos estocados por empresa:\n"
+            f"{companies_counter}"
+        )
+
+    @classmethod
+    def get_companies_counter(cls, products: list[Product]):
+        companies = [
+            product["nome_da_empresa"]
+            for product in products
+        ]
+
+        companies_counter = Counter(companies)
+
+        companies_string = ""
+
+        for company, qty in companies_counter.items():
+            companies_string += f"- {company}: {qty}\n"
+
+        return companies_string
