1d2d7ad74ce473cc8e0edf6e762f3a851c1cebb4
Pedro Papini <ppapinia@gmail.com>
2023-03-05 11:30:30 -0300

    Implement XML import

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import xml.etree.ElementTree as ET
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -10,8 +11,18 @@ class Inventory:
         with open(path_to_file, mode="r") as data:
             if path_to_file.endswith('.csv'):
                 products = list(csv.DictReader(data))
-            else:
+            elif path_to_file.endswith('.json'):
                 products = json.load(data)
+            else:
+                tree = ET.parse(data)
+                root = tree.getroot()
+                records = [child for child in root]
+                products = []
+                for record in records:
+                    product = {}
+                    for child in record:
+                        product[child.tag] = child.text
+                    products.append(product)
 
             if report_type == 'simples':
                 return SimpleReport.generate(products)
