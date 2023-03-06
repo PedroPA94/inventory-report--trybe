84b40546a58ba0af5814c66438f5b547ed5e3e81
Pedro Papini <ppapinia@gmail.com>
2023-03-06 16:17:41 -0300

    Add main function

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,30 @@
+import sys
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+
+
 def main():
-    pass
+    try:
+        _, path_to_file, report_type = sys.argv
+    except ValueError:
+        sys.stderr.write('Verifique os argumentos\n')
+    else:
+        importer = get_importer(path_to_file)
+        inventory = InventoryRefactor(importer)
+        report = inventory.import_data(path_to_file, report_type)
+        sys.stdout.write(report)
+
+
+def get_importer(path_to_file: str):
+    if path_to_file.endswith('.csv'):
+        return CsvImporter
+
+    if path_to_file.endswith('.json'):
+        return JsonImporter
+
+    if path_to_file.endswith('.xml'):
+        return XmlImporter
+
+    raise ValueError('Arquivo inválido')
